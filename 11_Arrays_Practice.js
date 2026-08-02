32211bb67a98142948285aa69ea9d6cd95c5916a
shashibhusan6 <singhashashibhusan6@gmail.com>
2026-08-02 19:35:39 +0530

    Added basic array question

--- 11_Arrays_Practice.js
+++ 11_Arrays_Practice.js
@@ -34,3 +34,43 @@
 // Q14. Find the average of all elements in an array.
 
 // Q15. Count the total number of elements in an array.
+
+
+
+/*
+=================================================
+        11_Arrays_Medium.js
+        MEDIUM LEVEL
+=================================================
+*/
+
+// Q16. Find the largest element in an array.
+
+// Q17. Find the smallest element in an array.
+
+// Q18. Count how many even numbers are present in an array.
+
+// Q19. Count how many odd numbers are present in an array.
+
+// Q20. Count positive numbers in an array.
+
+// Q21. Count negative numbers in an array.
+
+// Q22. Count zeros in an array.
+
+// Q23. Search for a given element in an array (Linear Search).
+
+// Q24. Check whether a given element exists in the array.
+
+// Q25. Find the index of a given element.
+
+// Q26. Print all even numbers from the array.
+
+// Q27. Print all odd numbers from the array.
+
+// Q28. Find the multiplication (product) of all array elements.
+
+// Q29. Copy one array into another array.
+
+// Q30. Merge two arrays into one array.
+
