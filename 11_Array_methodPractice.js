3fb510a705f3d4a64415503607fe3f3824eaf3ad
shashibhusan6 <singhashashibhusan6@gmail.com>
2026-08-05 19:41:08 +0530

    Added Medium Array method question

--- 11_Array_methodPractice.js
+++ 11_Array_methodPractice.js
@@ -35,3 +35,45 @@
 // Q14. Convert an array into a string using toString().
 
 // Q15. Perform push(), pop(), shift(), and unshift() on the same array and print the final array.
+
+
+
+
+
+/*
+=================================================
+        11_ArrayMethods_Medium.js
+        ARRAY METHODS PRACTICE
+             MEDIUM LEVEL
+=================================================
+*/
+
+// Q16. Replace the second element using splice().
+
+// Q17. Remove two elements starting from index 2 using splice().
+
+// Q18. Insert "Orange" at index 1 using splice().
+
+// Q19. Reverse an array using reverse().
+
+// Q20. Sort numbers in ascending order using sort().
+
+// Q21. Sort numbers in descending order using sort().
+
+// Q22. Fill an array with 0 using fill().
+
+// Q23. Fill only the first three elements with 100 using fill().
+
+// Q24. Copy the last two elements to the beginning using copyWithin().
+
+// Q25. Print every element using forEach().
+
+// Q26. Double every number using map().
+
+// Q27. Convert all names into uppercase using map().
+
+// Q28. Get all even numbers using filter().
+
+// Q29. Get all odd numbers using filter().
+
+// Q30. Create a new array containing squares of all numbers using map().
