766fd98f74fd1ad08ba33e033bf652d37359dfe0
shashibhusan6 <singhashashibhusan6@gmail.com>
2026-08-05 19:40:23 +0530

    Added easy Array method question

---
+++ 11_Array_methodPractice.js
@@ -0,0 +1,37 @@
+/*
+=================================================
+        11_ArrayMethods_Easy.js
+        ARRAY METHODS PRACTICE
+              EASY LEVEL
+=================================================
+*/
+
+// Q1. Create an array and add one element using push().
+
+// Q2. Add two elements at the end using push().
+
+// Q3. Remove the last element using pop().
+
+// Q4. Add one element at the beginning using unshift().
+
+// Q5. Remove the first element using shift().
+
+// Q6. Check whether 50 exists in an array using includes().
+
+// Q7. Check whether "Java" exists in an array using includes().
+
+// Q8. Find the index of 30 using indexOf().
+
+// Q9. Find the last occurrence of 20 using lastIndexOf().
+
+// Q10. Extract the first three elements using slice().
+
+// Q11. Extract the last two elements using slice().
+
+// Q12. Merge two arrays using concat().
+
+// Q13. Convert an array into a comma-separated string using join().
+
+// Q14. Convert an array into a string using toString().
+
+// Q15. Perform push(), pop(), shift(), and unshift() on the same array and print the final array.
